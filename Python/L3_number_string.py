284385d23ed15a12ffdc33ee596f3d2214cc4fff
salima5214 <mingming5214@gmail.com>
2021-12-14 03:09:11 -0500

    L3 number string

---
+++ Python/L3_number_string.py
@@ -0,0 +1,45 @@
+# L2: https://reurl.cc/82KGod
+
+x1 = 7/6
+x2 = 7//6
+x3 = 2**3
+x4 = 2**0.5
+x5 = 7%3
+
+print("integer division = {}\n double division = {}\n square = {}\n root = {}\n mod = {}".format(x1, x2, x3, x4, x5))
+
+print("###########################")
+
+# \ escape
+# """ can add next line
+
+print("new line:")
+s = """Hello
+
+
+world
+"""
+print(s)
+
+print("###########################")
+
+myString1 = "Hel\"lo"
+print(myString1)
+myString2 = "Hello"
+myString3 = "World"
+
+
+'''
+index  0 1 2 3 4 
+       W o r l d
+'''
+
+print("index access:")
+print(myString3[1:4])
+print(myString3[1:])
+print(myString3[:3])
+print("###########################")
+print("concatenate string:")
+print(myString2 + myString3)
+print("Hello "*5 + "World")
+
