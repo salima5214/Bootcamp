6b11759465a9886730234c679abcc26adcf95bdd
salima5214 <mingming5214@gmail.com>
2021-12-16 10:40:17 -0500

    L10

---
+++ Python/L10_function_args.py
@@ -0,0 +1,22 @@
+# L10: https://reurl.cc/bn0kly
+
+def divide(n1 = 10, n2 = 2):
+    print(n1/n2)
+
+divide(50, 2)
+divide(2, 50)
+divide(n2 = 2, n1 = 50)
+divide()
+
+print("#########")
+
+def avg(*ns):
+    print(ns)
+    sum = 0
+    for n in ns:
+        sum += n
+    print(sum/len(ns))
+
+#avg(3, 4)
+#avg(3, 5, 10)
+avg(1, 4, -1, -8)
