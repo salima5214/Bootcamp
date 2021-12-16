de0161c1fdc8727b1520b8fae5e499c305140509
salima5214 <mingming5214@gmail.com>
2021-12-16 10:31:10 -0500

    L9

---
+++ Python/L9_function_basic.py
@@ -0,0 +1,14 @@
+# L9: https://reurl.cc/zWxMNk
+
+
+def multiply1(n1, n2):
+    print(n1 * n2)
+    return
+
+def multiply2(n1, n2):
+    print(n1 * n2)
+    
+value1 = multiply1(3, 4)
+value2 = multiply2(3, 4)
+
+print(value1, value2)
