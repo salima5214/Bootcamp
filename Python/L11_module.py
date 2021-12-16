54fe2bc8ea8b84e413cb314c78d02413ce6a9475
salima5214 <mingming5214@gmail.com>
2021-12-16 11:08:55 -0500

    L11

---
+++ Python/L11_module.py
@@ -0,0 +1,13 @@
+# L11: https://reurl.cc/mvEGp9
+
+import sys 
+sys.path.append("modules")
+import L11_geometry 
+
+print(sys.platform)
+print(sys.maxsize)
+print(sys.path)
+
+print(L11_geometry.distance(1, 1, 5, 5))
+print(L11_geometry.slope(1, 1, 5, 5))
+
