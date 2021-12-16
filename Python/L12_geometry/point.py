b3c0525a67f5c8252e53113c84fb9249ebb14b30
salima5214 <mingming5214@gmail.com>
2021-12-16 11:20:24 -0500

    L12

---
+++ Python/L12_geometry/point.py
@@ -0,0 +1,5 @@
+def distance(x1, x2):
+    return (x1**2+x2**2)**0.5
+
+def slope(x1, y1, x2, y2):
+    return (y2-y1)/(x2-x1)
---
+++ Python/L12_package.py
@@ -0,0 +1,9 @@
+# L12: https://reurl.cc/Q6zjAb
+
+import L12_geometry.point
+
+print(L12_geometry.point.distance(3, 4))
+
+import L12_geometry.line
+
+print("slope = ", L12_geometry.line.slope(1, 1, 3, 3))
