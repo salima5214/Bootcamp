20b7acb8f8cc591ef8f64c3bd223f41bca741e4c
salima5214 <mingming5214@gmail.com>
2021-12-21 11:56:22 -0500

    Learn: craw url & pip install beautifulsoup4 & import bs4

---
+++ Python/L19_crawler.py
@@ -0,0 +1,31 @@
+# L19: https://reurl.cc/Xlo7X0
+
+# pip install beautifulsoup4
+import urllib.request as req
+
+url = "https://www.ptt.cc/bbs/movie/index.html"
+
+request = req.Request(url, headers={
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
+})
+
+with req.urlopen(request) as response:
+    data = response.read().decode( "utf-8" )
+print( data )
+
+# with req.urlopen(url) as response:
+#     data = response.read().decode( "utf-8" )
+# print( data ) 
+
+import bs4
+root = bs4.BeautifulSoup(data, "html.parser")
+print(root.title.string)
+
+# titles = root.find("div", class_ = "title")
+# print(titles.a.string)
+
+titles = root.find_all("div", class_ = "title")
+# print(titles)
+for title in titles:
+    if title.a != None:
+        print(title.a.string)
