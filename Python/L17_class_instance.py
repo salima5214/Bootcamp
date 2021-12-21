5d71711a1c15db4f7f246aa774f475a63331d9cf
salima5214 <mingming5214@gmail.com>
2021-12-21 11:10:08 -0500

    Learn: class instance attributes

---
+++ Python/L17_class_instance.py
@@ -0,0 +1,12 @@
+# L17:https://reurl.cc/0xL53k
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+p1 = Point(3, 4)
+print( p1.x, p1.y )
+
+p2 = Point(5, 6)
+print( p2.x, p2.y )
